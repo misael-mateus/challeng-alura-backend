a89d185c9723e34ff5ac7ad8042977e87279471f
misael <misaelmateus.java@gmail.com>
2022-02-11 08:35:25 -0400

    Added: Adicionado Spring ao Projeto

---
+++ src/main/java/br/com/challenge/api_financeiro/ApiFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.api_financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinanceiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFinanceiroApplication.class, args);
+    }
+
+}
